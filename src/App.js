3e5e756683b8125ae97ed05513e359ba740e6d89
Jim Dios <jbryan3091@gmail.com>
2021-04-04 01:38:22 -0500

    First Commit basic react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+/* function Helloworld(props) {
+  return (
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+    </div>
+  );
+} */
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show : !this.state.show})
+
+  }
+
+  render() {
+    if (this.state.show) {
+    return (
+      <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.mytext}
+        <button onClick={this.toggleShow}>Toggle Show</button>
+      </div>
+    )
+    } else{
+      return <h1>
+        There are not elements
+        <button onClick={this.toggleShow}>
+          Toggle Show
+        </button>
+        </h1>
+    }
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component:
+      <Helloworld mytext="Hello Jim" subtitle="Loremp ipsum" />
+      <Helloworld mytext="Hola Mundo" subtitle="component 02" />
+      <Helloworld mytext="Hello!" subtitle="component 03" />
     </div>
   );
 }
